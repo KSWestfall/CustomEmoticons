9582f45c6728c82b9a5804abda04694686f61c6a
Robert Snyder <Robert.Snyder@pressganey.com>
2016-08-25 09:34:45 -0400

    modified the script to kill the jabber process, update to the latest then restart jabber: TODO: add git check to see if it is already up to date.

--- autofetchCustomEmoticons.ps1
+++ autofetchCustomEmoticons.ps1
@@ -1,5 +1,5 @@
+Stop-Process -Name CiscoJabber
 $CustomEmoticonPath = "C:\Program Files (x86)\Cisco Systems\Cisco Jabber\CustomEmoticons"
 cd $CustomEmoticonPath
-git fetch --all
-git pull
-#Read-Host "Press any key to continue"
+. "C:\Program Files\Git\bin\git.exe" pull origin master
+. "C:\Program Files (x86)\Cisco Systems\Cisco Jabber\CiscoJabber.exe"
